f1f3373817b19c115298231e72caac7ee7247830
Abhirup <argang05@gmail.com>
2024-04-19 21:32:14 +0530

    updated again

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const connectToDB = async () => {
             console.log("Using existing connection");
             return;
         }
-        const db = await mongoose.connect(process.env.MONGO);
+        const db = await mongoose.connect(process.env.MONGO.toString());
         connection.isConnected = db.connections[0].readyState;
     } catch (error) {
         console.log(error);
